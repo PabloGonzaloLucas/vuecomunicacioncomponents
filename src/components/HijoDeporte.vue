1ea4a52ed2c4e05531ca3e6c73f91b0629518027
Pablo <pablogonz.2204@gmail.com>
2025-10-27 14:14:06 +0100

    first commit

---
+++ src/components/HijoDeporte.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h4 style="color:red">{{nombreDeporte}}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "HijoDeporte",
+    props: ["nombreDeporte"],
+    mounted() {
+        console.log(this.nombreDeporte)
+    }
+}
+</script>
+
+<style>
+
+</style>
